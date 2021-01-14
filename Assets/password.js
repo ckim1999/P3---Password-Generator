8781a939da899c53cb2dd87616a9aab10f98b95a
Christopher Kim <chrisnkim1999@gmail.com>
2021-01-14 17:31:14 -0500

    added comments to the javascript file

---
+++ Assets/password.js
@@ -0,0 +1,12 @@
+// list of password characters
+
+// lowercase
+// uppercase
+// numeric
+// special characters
+
+// provide a length for the password (8 - 128 characters)
+
+// input is validated
+
+// password is shown
